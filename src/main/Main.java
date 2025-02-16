657756028340a5884003b9d5d2512faa50b84d8a
Iswed <milanastasenko5@gmail.com>
2025-02-16 21:02:39 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,44 @@
+package main;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import server.AccountServer;
+import server.AccountServerController;
+import server.AccountServerControllerMBean;
+import servlets.AccountServerServlet;
+
+import javax.management.*;
+import java.lang.management.ManagementFactory;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AccountServer accountServer = new AccountServer(10);
+
+        AccountServerControllerMBean serverStatistics = new AccountServerController(accountServer);
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("Admin:type=AccountServerController.usersLimit");
+        mbs.registerMBean(serverStatistics, name);
+
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new AccountServerServlet(accountServer)), AccountServerServlet.PAGE_URL);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("static");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
